21201a78a8d182cc0d818b051ac8c7b59cd625e1
khahv <khaep92@gmail.com>
2026-01-10 21:08:38 +0700

    add confirm button

---
+++ import.tf
@@ -0,0 +1,24 @@
+# import {
+#   to = azurerm_resource_group.rg
+#   id = "/subscriptions/88b52e56-8792-40a4-ba08-5717bc6d797a/resourceGroups/rg-tf-vm-lab"
+# }
+# import {
+#   to = azurerm_virtual_network.vnet
+#   id = "/subscriptions/88b52e56-8792-40a4-ba08-5717bc6d797a/resourceGroups/rg-tf-vm-lab/providers/Microsoft.Network/virtualNetworks/vnet-tf-lab"
+# }
+# import {
+#   to = azurerm_subnet.subnet
+#   id = "/subscriptions/88b52e56-8792-40a4-ba08-5717bc6d797a/resourceGroups/rg-tf-vm-lab/providers/Microsoft.Network/virtualNetworks/vnet-tf-lab/subnets/subnet-1"
+# }
+# import {
+#   to = azurerm_public_ip.pip
+#   id = "/subscriptions/88b52e56-8792-40a4-ba08-5717bc6d797a/resourceGroups/rg-tf-vm-lab/providers/Microsoft.Network/publicIPAddresses/pip-vm-lab"
+# }
+# import {
+#   to = azurerm_network_interface.nic
+#   id = "/subscriptions/88b52e56-8792-40a4-ba08-5717bc6d797a/resourceGroups/rg-tf-vm-lab/providers/Microsoft.Network/networkInterfaces/nic-vm-lab"
+# }
+# import {
+#   to = azurerm_linux_virtual_machine.vm
+#   id = "/subscriptions/88b52e56-8792-40a4-ba08-5717bc6d797a/resourceGroups/rg-tf-vm-lab/providers/Microsoft.Compute/virtualMachines/vm-tf-lab"
+# }
